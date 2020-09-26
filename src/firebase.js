0b847e7f356c3f6122b2a1b5f229e0120f0a8281
Issa Yusuf <abdiy1995@gmail.com>
2020-09-26 16:21:33 +1000

    Added untracked files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAXxvLOogzpKIc29t1FisSP85YY78L2zw",
+    authDomain: "whatsapp-clone-mern-824f0.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-mern-824f0.firebaseio.com",
+    projectId: "whatsapp-clone-mern-824f0",
+    storageBucket: "whatsapp-clone-mern-824f0.appspot.com",
+    messagingSenderId: "783370355084",
+    appId: "1:783370355084:web:a5c8d007ac66e9b446d6a9"
+  };
